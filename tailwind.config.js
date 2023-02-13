aab5c89c224b6198ebc86eeae42da146b393fbfb
Shrey <shreybirmiwal@gmail.com>
2023-02-12 19:02:14 -0600

    try 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
   },
   plugins: [],
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-}
 }
