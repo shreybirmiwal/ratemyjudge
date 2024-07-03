0d7e4da8289903e4d55a4d913a554d6758f3dfd2
Shrey Birmiwal <shreybirmiwal@gmail.com>
2024-07-03 07:44:25 -0500

    updated about and removed directory

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,24 +1,31 @@
-import React from 'react'
-import Navbar from '../Components/Navbar.js'
+import React from 'react';
+import Navbar from '../Components/Navbar.js';
 
 function About() {
+  return (
+    <div className="w-full h-screen relative flex flex-col">
+      <Navbar />
+      <div className="flex-grow flex flex-col items-center justify-center bg-gray-900/30">
+        <h1 className="text-4xl font-bold mb-4 text-center">About RateMyJudge.xyz</h1>
+        <h2 className="text-xl font-semibold mb-6 text-center">Leveling the Playing Field in Public Forum Debate</h2>
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-3xl text-gray-800">
 
-    return (
-    <div className='w-full h-screen relative'>
-        <Navbar/>
-
-        <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
-        <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-black p-4'>
-          
-          <h1>About</h1>
-          <h2 className='py-4'></h2>
-          <p>This website was created by Shrey Birmiwal in an effort to make debate a more fair....... To be finished </p>
-  
+          <p className="mb-4">
+            RateMyJudge.xyz was created to make debate rounds, especially in Public Forum, fairer and more transparent. Many high school debate judges have biases toward certain styles of debating - some prefer speed, others favor slow and deliberate arguments, some like theoretical debates, and others enjoy progressive arguments.
+          </p>
+          <p className="mb-4">
+            Sometimes, judges do not have a paradigm posted or they judge differently in actuality compared to their posted paradigm. This can create an uneven playing field and confusion for debaters.
+          </p>
+          <p className="mb-4">
+            Our community-run voting center offers open-source disclosure of judges' information. This transparency helps make debate safer, more educational, and fair for everyone involved.
+          </p>
+          <p>
+            Created by Shrey Birmiwal, RateMyJudge.xyz is dedicated to improving the debate experience by providing valuable insights and fostering a fair environment for all participants.
+          </p>
         </div>
       </div>
-  )
-
-
+    </div>
+  );
 }
 
-export default About
+export default About;
--- src/pages/Directory.js
+++ src/pages/Directory.js
@@ -11,44 +11,46 @@ function Directory() {
     getData()
   }, []);
 
-  const getData = async () =>{
-      const temp = [];
-      const querySnapshot = await getDocs(collection(db, "votes"));
-      querySnapshot.forEach((doc) => {
-        temp.push(doc.id)
-      });
-      
-      setData(temp)
+  const getData = async () => {
+    const temp = [];
+    const querySnapshot = await getDocs(collection(db, "votes"));
+    querySnapshot.forEach((doc) => {
+      temp.push(doc.id)
+    });
+
+    setData(temp)
   }
-  
+
 
   return (
     <div className='h-screen relative'>
-        <Navbar/>
+      <Navbar />
 
-        <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
+      <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
+      <h1 className='p-16'>Directory</h1>
 
-        {data.length != 0 && (
+      {data.length != 0 && (
         <div className='absolute top-0 -mt-10  h-full flex flex-col justify-center ml-5 text-black p-4'>
-            <h1 className='mb-5'>Directory</h1>
 
 
-          {data.map((value, key) => {
-            
-            const arr = value.split(' ')
+          <div className='bg-pink-600'>
+            {data.map((value, key) => {
 
-            const temp = "/Judges/" + arr[0]+"/"+arr[1]
+              const arr = value.split(' ')
 
-            return (
-              <a className="" key={value} href={temp}>
-                <p>{value} </p>
-              </a>
-            );
-          })}
+              const temp = "/Judges/" + arr[0] + "/" + arr[1]
 
+              return (
+                <a className="" key={value} href={temp}>
+                  <p>{value} </p>
+                </a>
+              );
+            })}
+
+          </div>
         </div>
+
       )}
-        
 
     </div>
   )
